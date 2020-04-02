5c0482226ed750080f563061322286ad3169992a
github username <github email address>
2020-04-02 17:03:49 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React, { useEffect } from 'react';
+
+// export default function App() {
+//     useEffect(() => {
+//         function fetchItem() {
+//             fetch("http://api.open-notify.org/astros.json")
+//                 .then(response => response.json())
+//                 .then(json => console.log(json))
+//         }
+//         fetchItem()
+
+//     })
+//     return (
+//         <div>This isApp</div>
+//     )
+// }
+
+export default class App extends React.Component {
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+            .then(response => response.json())
+            .then(json => console.log(json))
+    }
+
+    render() {
+        return (
+            <div>Hello</div>
+        )
+    }
+}
+
